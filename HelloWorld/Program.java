9d09e2a41eb71e3d8bba744e929bd6cee705747c
Андрей Коломытов <dos@Andrejs-MacBook.local>
2020-11-25 20:37:51 +0300

    Test

---
+++ HelloWorld/Program.java
@@ -0,0 +1,11 @@
+public class Program{ 
+    public static void main (String args[]){
+        var x = """
+        Вот мысль, которой весь я предан,
+        Итог всего, что ум скопил.
+        Лишь тот, кем бой за жизнь изведан,
+        Жизнь и свободу заслужил.
+        """;
+        System.out.println(x);
+    }
+}
