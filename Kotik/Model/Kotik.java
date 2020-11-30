a2dff3a3d465fb91fa7e0bb53154af9fedfeef77
Андрей Коломытов <dos@Andrejs-MacBook.local>
2020-11-30 18:29:03 +0300

    kotiks

--- Kotik/Model/Kotik.java
+++ Kotik/Model/Kotik.java
@@ -5,7 +5,7 @@ public class Kotik {
     private int weight;
     private String name;
     private String meow;
-    private static int satiety;
+    private static int satiety = 10;
     private static int livestock = 0;
 
     public int getPrettiness(){
@@ -28,79 +28,58 @@ public class Kotik {
         return this.satiety;
     }
 
-    public static void main(String args[]) {
-    Object Kotik = new Kotik.setKotik();
+    public static void main(String args[]){
     System.out.println(livestock + " kotiks were created.");
     }
     
-    static int eat() {
-        if (satiety > 0){
-            System.out.println("omnomnom... =^_^=");
-            return 1;
-        }
-        else {
-            Kotik.eat();
-            return 0;
+    void eat(){
+        System.out.println(name + " is having a feast! omnomnom... =^_^=");
+        satiety++;
+    }
+
+    void sleap(){
+        switch(satiety){
+            case 0:
+                System.out.println(name + " is hungry and won'n sleep! Feed the damn kotik!");
+                break;
+            default:
+            System.out.println(name + " is having a nap! ...sweet...dreams...full...of...mice...");
+       
         }
     }
 
-    static int sleap() {
-        if (satiety > 0){
-            System.out.println("...sweet...dreams...full...of...mice...");
-            return 1;
-        }
-        else {
-            Kotik.eat();
-            return 0;
-        }
-    }
-
-    static int stompFloor() {
-        if (satiety > 0){
-            System.out.println("TIGIDIK-TIGIDIK-TIGIDIK!!!!");
-            return 1;
-         }
-         else {
-             Kotik.eat();
-             return 0;
+    void stompFloor(){
+        switch(satiety){
+            case 0:
+                System.out.println(name + " is hungry and won't tigidik! Feed the damn kotik!");
+                break;
+            default:
+            System.out.println(name + ": TIGIDIK-TIGIDIK-TIGIDIK!!!!");
          }
     }
 
-    static int purr() {
-        if (satiety > 0){
-            System.out.println("Prrrr...Prrrr....Prrrr...");
-            return 1;
-         }
-         else {
-             Kotik.eat();
-             return 0;
+    void purr(){
+        switch(satiety){
+            case 0:
+                System.out.println(name + " is hungry and won't purr! Feed the damn kotik!");
+                break;
+            default:
+            System.out.println(name + ": Prrrr...Prrrr....Prrrr...");
          }
     }
     
 
-    static int tearSofa() {
-        if (satiety > 0){
-            System.out.println("Scratch.. Scrath... NOT MY FABULUOS SOFA!!!");
-            return 1;
-         }
-         else {
-             Kotik.eat();
-             return 0;
+    void tearSofa(){
+        switch(satiety){
+            case 0:
+                System.out.println(name + " is hungry and won't tear your funriture! Feed the damn kotik!");
+                break;
+            default:
+            System.out.println(name + " is tearing your furnitute. Scratch.. Scrath... NOT MY FABULUOS SOFA!!!");
          }
     }
+  
     
-    // Конструктор без параметров, он идентичен конструктору по умолчанию,
-    // который сюда бы подставила Java,
-    // Но она его не подставит, по той причине,
-    // что ниже определяется другой (перегруженный) конструктор, с параметрами.
-    public Kotik() {
-    }
-
-    /*
-     * Указатель this можно использовать для вызова перегруженного конструктора
-     */
-   /*  public Kotik(int weight, String name) {
-        this(5, weight, name, "Мяу!", 1); */ 
 
     public Kotik(int prettiness, int weight, String name, String meow, int satiety) {
         this.prettiness = prettiness;
@@ -108,8 +87,9 @@ public class Kotik {
         this.name = name;
         this.meow = meow;
         this.satiety = satiety;
+        livestock++;
     }
-
+  
 
     void setKotik(int prettiness, String name, int weight, String meow, int satiety) {
         this.prettiness = prettiness;
