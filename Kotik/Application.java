24d99660912d3c2dc1992213d9ee158cf1f9112e
Андрей Коломытов <dos@Andrejs-MacBook.local>
2020-11-30 21:31:51 +0300

    123

--- Kotik/Application.java
+++ Kotik/Application.java
@@ -2,13 +2,18 @@ import Model.*;
 
 public class Application{
 public static void main(String args[]) {
-    Kotik kotik_1 = new Kotik(1, "Vasya", 8, "Maou");
+    Kotik kotik_1 = new Kotik(1, "Barsik", 8, "Mau");
+    
     Kotik kotik_2 = new Kotik();
-    kotik_2.setKotik(2, "Barsik", 4, "Marrr!");
+    kotik_2.setKotik(2, "Gudinny", 4, "Meaw");
 
     System.out.println();
     kotik_2.liveAnotherDay();
+    
     System.out.println("\nKotik " + kotik_2.getName() + " weights " + kotik_2.getWeight() + " kilos");
-    System.out.println("\nkotik_1.getMeow().equals(kotik_2.getMeow())\n");
+    
+    System.out.println(kotik_1.getMeow().equals(kotik_2.getMeow()));
+
+    System.out.prinln(Kotik.class.getLive)
     }
 }
